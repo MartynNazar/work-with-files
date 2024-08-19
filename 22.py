6cc79307e431df9edea29094cc8749c872cb48aa
breadsrt <orestmykhatsko@gmail.com>
2024-08-19 16:30:39 +0300

    work with files

---
+++ 22.py
@@ -0,0 +1,16 @@
+import json
+
+with open("bd (1).json", 'r', encoding="utf-8") as file:
+    data = json.load(file)
+    print(data)
+
+name = input("Введіть імя учня")
+s = 0
+count = 0
+
+for element in data:
+     if name == element["імя"]:
+         s += element['оцінка']
+         count += 1
+
+print(s/count)
